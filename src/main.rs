71f37724ee555ce974ebd3593b6097e6a41de7cb
William <atiwiex@gmail.com>
2018-09-12 13:15:14 -0400

    Update version

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use std::str;
 fn main() {
     
     let matches = App::new("lyrics")
-        .version("0.1.0")
+        .version("1.0.0")
         .author("William <me@stylite.me>")
         .about("Genius lyric grabber")
         .arg(Arg::with_name("URL")
